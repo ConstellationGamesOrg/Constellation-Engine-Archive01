6008cbdecaf90880cb2bf7670eabcea3650a1429
EmperorNecro <76569260+EmperorNecro@users.noreply.github.com>
2022-06-26 12:59:11 +0100

    Updated graphics.c

--- src/core/graphics/graphics.c
+++ src/core/graphics/graphics.c
@@ -17,7 +17,8 @@ int core_graphics_graphicsInit(struct graphicsSettings* graphicsSettings) {
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	}
-
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #ifndef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
