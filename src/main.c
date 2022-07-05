39f9dec261f22991127857ef794831a0e985941f
CMDR-Piboy314 <92357316+CMDR-Piboy314@users.noreply.github.com>
2022-07-04 21:08:13 -0400

    Revert 'the 4D update'

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ int main() {
 	float vertices[] = {
 	//    X      Y     Z      X     Y
 		 0.5f,  0.5f, 0.0f,  1.0f, 1.0f,   // Top right
-		 0.5f, -0.5f, 0.5f,  1.0f, 0.0f,   // Bottom right
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f,   // Bottom right
 		-0.5f, -0.5f, 0.0f,  0.0f, 0.0f,   // Bottom left
 		-0.5f,  0.5f, 0.0f,  0.0f, 1.0f    // Top left
 	};
